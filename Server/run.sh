5d021b2c860737daa5685f8c47ba97ef31cbeece
Ben Shutt <ben@3sidedcube.com>
2023-08-20 19:38:42 +0100

    Setup server.c

---
+++ Server/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#
+# Script: run.sh
+# Usage: ./run.sh
+#
+# Clean, make, and run the program
+#
+
+# Set defaults
+set -o nounset -o errexit -o errtrace -o pipefail
+
+# The directory of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Path to the executable
+EXE="${SCRIPT_DIR}/server"
+
+# Run
+make clean && make && ${EXE} && make clean
