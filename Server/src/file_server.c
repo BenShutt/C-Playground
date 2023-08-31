f8b33bc08b735ffb6d9cde6c3c31e33cb12c334a
Ben Shutt <ben@3sidedcube.com>
2023-08-31 22:22:23 +0100

    File server WIP

--- Server/src/file_server.c
+++ Server/src/file_server.c
@@ -1,12 +1,26 @@
 #include "file_server.h"
 #include "check.h"
 #include "request_parts.h"
+#include <string.h>
+
 #include "request.h"
+#include "check.h"
+#include "file.h"
+
+#define URL_PREFIX "/Users/benshutt/Desktop"
 
 int write_request(Request *request) // Private 
 {
-    Request_print(request);
-    return 0;
+    check(URL_PREFIX != NULL, "URL prefix.");
+    
+    char url[100];
+    int rc = sprintf(url, "%s/%s", URL_PREFIX, request->file_name);
+    check(rc > 0, "Failed to build URL.");
+
+    return write_file((uint8_t *)request->data, request->size, url); // TODO
+
+error:
+    return -1;
 }
 
 char *file_server_handle(const char *message)
@@ -19,7 +33,8 @@ char *file_server_handle(const char *message)
     check(request_parts != NULL, "Failed to initialize RequestParts.");
 
     request = Request_init(request_parts);
-    check(request != NULL, "Failed to initialize Request.");
+    check(request != NULL, "Failed to initialize Request."); 
+    Request_print(request);
 
     int rc = write_request(request);
     check(rc == 0, "Failed to write file.");
