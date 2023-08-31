337a76c5804062c19258b3773921b9f3a290965a
Ben Shutt <ben@3sidedcube.com>
2023-08-31 08:53:44 +0100

    Update request.c

--- Server/src/request.c
+++ Server/src/request.c
@@ -1,4 +1,5 @@
 #include <cjson/cJSON.h>
+#include <stdbool.h>
 
 #include "request.h"
 #include "check.h"
@@ -9,12 +10,23 @@ FileRequest *FileRequest_init(const char *message)
     check(message != NULL, "Invalid argument");
 
     json = cJSON_Parse(message);
+    check(json != NULL, "Invalid JSON.");
+
+    cJSON *file_name_json = cJSON_GetObjectItemCaseSensitive(json, "file_name");
+    bool file_name_rc = cJSON_IsString(file_name_json) && file_name_json->valuestring != NULL;
+    check(file_name_rc, "Invalid or missing 'file_name' key.");
+    char *file_name = file_name_json->valuestring; // TODO zero terminator?
+
+    cJSON *size_json = cJSON_GetObjectItemCaseSensitive(json, "size");
+    bool size_rc = cJSON_IsNumber(size_json) && size_json->valueint > 0;
+    check(size_rc, "Invalid or missing 'size' key.");
+    size_t size = (size_t)file_name_json->valueint;
 
     FileRequest *request = malloc(sizeof(FileRequest));
     check_memory(request);
 
-    request->file_name = NULL;
-    request->size = 0;
+    request->file_name = file_name;
+    request->size = size;
 
     if(json) cJSON_Delete(json);
     return request;
