b8473f882356154a2da5ad3c85dc88673bd6e55d
Ben Shutt <ben@3sidedcube.com>
2023-08-31 15:25:54 +0100

    Make file.h and file.c

---
+++ Server/src/file.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "file.h"
+#include "check.h"
+
+int write_file(unsigned char bytes[], size_t size, const char *url)
+{
+    FILE *file = NULL;
+    check(url != NULL, "Invalid argument.");
+
+    file = fopen(url, "wb");
+    check(file != NULL, "Failed to open file '%s'.", url);
+
+    size_t rc = fwrite(bytes, size, 1, file);
+    check(rc >= 1, "Failed to write to file '%s'.", url);
+
+    fclose(file);
+    return 0;
+
+error:
+    if(file) fclose(file);
+    return -1;
+}
---
+++ Server/src/file.h
@@ -0,0 +1,8 @@
+#ifndef __file_h__
+#define __file_h__
+
+#include <stdlib.h>
+
+int write_file(unsigned char bytes[], size_t size, const char *url);
+
+#endif
